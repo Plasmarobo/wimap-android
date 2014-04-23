96ac81cb6f0810d356c1b4dbecd9fbc5a442f974
Austen Higgins-Cassidy <plasmarobo@gmail.com>
2014-04-23 10:22:58 -0400

    Improved distance model
    Maximum observed accuracy (3.7m, manual)

--- Distance_Model/distance_model.rb
+++ Distance_Model/distance_model.rb
@@ -1,56 +1,59 @@
-require 'math'
+require 'csv'
 
-class distance_model
+class DistanceModel
 	PI = 3.14
 	
 
-	def distance(dBm,f, PTx)
+	def distance(dBm,f, ptx)
 		hz = f *(10**6)
 		c = (3*10**8)
 		frequency_attenuation =  20*Math.log10(c/hz)-20*Math.log10(4*PI)
-		jitter = 6*(Math.rand-0.5)
-		GTx = 2.3
-		GRx = 2.3
-		effective_power = PTx + GTx + GRx - jitter
+		jitter = 6*(Random.rand-0.5)
+		gtx = 2.3
+		grx = 2.3
+		effective_power = ptx + gtx + grx - jitter
 		n = 5
 		multipath_compensation = (10*n)
-		distance = 10**(effective_power-dBm)/multipath_compensation);
+		distance = 10**((effective_power-dBm)/multipath_compensation)
 	end
 
 	def lowpass(width, data_set, amount=2)
 
 	end
 
-	def find_PTx(power_map)
-		PTx = 75
+	def find_ptx(power_map)
+		ptx = 75.0
+		sq_error = 2500
+		min_error = 25
 		error = 2500
 		iteration = 0
 		iteration_limit = 15000
-		adjustment_rate = 1/3
-		while(error > 3 && iteration < iteration_limit)
+		adjustment_rate = 1.0/3.0
+		while(sq_error > min_error && iteration < iteration_limit)
 			if iteration > 1
 				if error > 0
-					PTx -= PTx * adjustment_rate
+					ptx += ptx * adjustment_rate
 				else
-					PTx += PTx * adjustment_rate
+					ptx -= ptx * adjustment_rate
 				end
 			end
-			puts "Guess: #{PTx}"
+			#puts "Guess: #{ptx}"
 			error = 0
 			distance_error = {}
 			power_map.each_pair do |(distance, dBms)|
 				distance_error[distance] = 0
 				dBms.each do |dBm|
-					distance_error[distance] += distance-self.distance(dBm, 2400, PTx)
+					distance_error[distance] += distance-self.distance(dBm.to_f, 2400, ptx)
 				end
 				distance_error[distance] = distance_error[distance]/dBms.size
 			end
 			distance_error.each_pair {|(distance, avg_error)| error += avg_error}
 			error = error / distance_error.size
-			puts "Average Error: #{error}"
+			sq_error = error ** 2
+			#puts "Average Error: #{error}"
 			iteration += 1
 		end
-		PTx
+		ptx
 	end
 
 	def parse_powermap_csv(filename)
